912caf994550d105af80cc8ceff13ad6c97417b5
henthe-5@student.ltu.se <henthe-5@student.ltu.se>
2019-03-12 22:36:34 +0100

    Added interrupt for DMA2_stream0 to read adc conversions

--- Project/Software/src/main.rs
+++ Project/Software/src/main.rs
@@ -29,7 +29,7 @@ use channel::Channel;
 use dma::{CircBuffer, Dma2Stream0};
 
 const FREQUENCY: Hertz = Hertz(4000);
-const ADCFREQUENCY: Hertz = Hertz(1);
+const ADCFREQUENCY: Hertz = Hertz(8);
 const N: usize = 2;
 // Our error type
 #[derive(Debug)]
@@ -45,17 +45,16 @@ const APP: () = {
     static mut DMA2: DMA2 = ();
     // static mut ADC: adc::Adc = ();
     static mut BUFFER: CircBuffer<'static, [u16; N], Dma2Stream0> = CircBuffer::new([[0; N]; 2]);
-    #[init(schedule = [read_adc], resources = [BUFFER])]
+    #[init(resources = [BUFFER])]
     fn init() {
         let rcc = device.RCC;
         let dma2 = device.DMA2;
         let adc1 = device.ADC1;
-        let tim2 = device.TIM2;
         let tim1 = device.TIM1;
-        let tim3 = device.TIM3;
-        let tim4 = device.TIM4;
-        let mut pwm = pwm::Pwm(&tim2);
+        let tim2 = device.TIM2;
 
+        //Enable pwm for driving the piezo speaker, tim2 channel 1 = PA15
+        let mut pwm = pwm::Pwm(&tim2);
         let c = &Channel::_1;
         pwm.init(
             FREQUENCY.invert(),
@@ -69,6 +68,9 @@ const APP: () = {
         pwm.set_duty(*c, pwm.get_max_duty() / 2);
         pwm.enable(*c);
 
+        //Enable ADC converting on adc channel IN_0 (PA0) and IN_1 (PA1), uses pwm to trigger converting and uses DMA2_STREAM0
+        //interrupt to print values from the buffer.
+        //Todo, possibly enable analog watchdog for adc channel 1 (pulse) to generate interrupt on high values to count pulses.
         let c2 = &Channel::_2;
         let mut pwm2 = pwm::Pwm(&tim1);
         pwm2.init(
@@ -80,17 +82,13 @@ const APP: () = {
             &device.GPIOC,
             &rcc,
         );
-        
         let adc = adc::Adc(&adc1, &tim1);
-
         adc.init(&dma2, &rcc);
         adc.enable_input(adc::AdcChannel::_0, 1, &device.GPIOA, &device.GPIOB, &device.GPIOC);
         adc.enable_input(adc::AdcChannel::_1, 2, &device.GPIOA, &device.GPIOB, &device.GPIOC);
         adc.enable();
         adc.start(resources.BUFFER, &dma2, &mut pwm2).unwrap();
        
-        schedule.read_adc(Instant::now() + (12_000_000).cycles()).unwrap();
-        
         ITM = core.ITM;
         DMA2 = dma2;
         
@@ -109,44 +107,23 @@ const APP: () = {
         let stim = &mut resources.ITM.stim[0];
         iprintln!(stim, "Det fungerar!");
 
-
         schedule.trace(Instant::now() + (16_000_000).cycles()).unwrap();
     }
 
-    //Todo, implement this to trigger on interrupt when buffer is readable or on adc conversion.
-    #[task(resources = [ITM, BUFFER, DMA2], schedule = [read_adc])]
-    fn read_adc() {
+    #[interrupt(resources = [ITM, BUFFER, DMA2])]
+    fn DMA2_STREAM0() {
         let stim = &mut resources.ITM.stim[0];
         match resources.BUFFER.read(resources.DMA2, |x| {
                 let buf: [u16; N] = x.clone();
                 buf
             }) {
-                Err(_) => {
-                    iprintln!
-                    (stim, "Error reading buffer");
-                },
+                Err(_) => cortex_m::asm::bkpt(),
                 Ok(b) => {
-                    iprintln!(stim, "Temp = {}, Pulse = {}", b[0], b[1]);
+                    iprintln!(stim, "{}, {}", b[0], b[1]);
                 }
             }
-        schedule.read_adc(Instant::now() + (12_000_000).cycles()).unwrap();
     }
-    // Set of interrupt vectors, free to use for RTFM tasks
-    // 1 per priority level suffices
 
-    // #[interrupt]
-    // fn ADC() {
-    //     let stim = &mut resources.ITM.stim[0];
-    //     match resources.BUFFER.read(resources.DMA2, |x| {
-    //             let buf: [u16; N] = x.clone();
-    //             buf
-    //         }) {
-    //             Err(_) => cortex_m::asm::bkpt(),
-    //             Ok(b) => {
-    //                 iprintln!(stim, "{}, {}, {}, {}, {}, {}", b[0], b[1], b[2], b[3], b[4], b[5]);
-    //             }
-    //         }
-    // }
     extern "C" {
         fn EXTI0();
     }
