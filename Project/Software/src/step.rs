df1e50a4dbf394c39fc6e08653d3be57390aa6cb
henthe-5@student.ltu.se <henthe-5@student.ltu.se>
2019-03-26 13:57:54 +0100

    Added another step

--- Project/Software/src/step.rs
+++ Project/Software/src/step.rs
@@ -9,7 +9,7 @@ pub struct Step {
     accel_ring_x: [f32; ACCEL_RING_SIZE],
     accel_ring_y: [f32; ACCEL_RING_SIZE],
     accel_ring_z: [f32; ACCEL_RING_SIZE],
-    vel_ring_counter: u32,
+    vel_ring_counter: usize,
     vel_ring: [f32; VEL_RING_SIZE],
     last_step_time_ns: u32,
     old_velocityestimate: u32,
@@ -23,14 +23,14 @@ impl Step {
             accel_ring_x: [0f32; ACCEL_RING_SIZE],
             accel_ring_y: [0f32; ACCEL_RING_SIZE],
             accel_ring_z: [0f32; ACCEL_RING_SIZE],
-            vel_ring_counter: 0u32,
+            vel_ring_counter: 0usize,
             vel_ring: [0f32; VEL_RING_SIZE],
             last_step_time_ns: 0u32,
             old_velocityestimate: 0u32,
         }
     }
 
-    pub fn calc_step(&mut self, x_g: f32, y_g: f32, z_g: f32) -> bool {
+    pub fn calc_step(&mut self, time_ns: f32, x_g: f32, y_g: f32, z_g: f32) -> bool {
         let mut current_accel = [0f32; 3];
         current_accel[0] = x_g;
         current_accel[1] = y_g;
@@ -57,18 +57,17 @@ impl Step {
         world_z[2] = world_z[2] / normalization_factor;
  
         let current_z = Filter::dot(&world_z, &current_accel) - normalization_factor;
-   
-    // float currentZ = SensorFilter.dot(worldZ, currentAccel) - normalization_factor;
-    // velRingCounter++;
-    // velRing[velRingCounter % VEL_RING_SIZE] = currentZ;
+        self.vel_ring_counter = self.vel_ring_counter.wrapping_add(1);
+        
+        self.vel_ring[self.vel_ring_counter % VEL_RING_SIZE] = current_z;
  
-    // float velocityEstimate = SensorFilter.sum(velRing);
+        let velocity_estimate = Filter::sum(self.vel_ring);
  
-    // if (velocityEstimate > STEP_THRESHOLD && oldVelocityEstimate <= STEP_THRESHOLD
-    //     && (timeNs - lastStepTimeNs > STEP_DELAY_NS)) {
-    //   listener.step(timeNs);
-    //   lastStepTimeNs = timeNs;
-    // }
+        if (velocity_Estimate > self.step_treshold && self.old_velocity_estimate <= self.step_treshold
+            && (timeNs - lastStepTimeNs > STEP_DELAY_NS)) {
+        listener.step(timeNs);
+        lastStepTimeNs = timeNs;
+        }
     // oldVelocityEstimate = velocityEstimate;
       true
     }
